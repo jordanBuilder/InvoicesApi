a9be8da74db076490585d86dd91ab3e5c89268dd
jordan Tomegah <tomegahjordan81@gmail.com>
2024-05-26 16:08:43 +0000

    initialisation du projet : creation du model Invoice

---
+++ app/Models/Invoice.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Invoice extends Model
+{
+    use HasFactory;
+    //fillable definit quels attributs peuvent être assignés en masse dans les methodes qu'on utilise pout agir sur la BD
+    // si on ne met pas fillable, les attributs du model ne pourront pas être assignées en masse genre ça là:
+    // User::create([
+    // 'name' => "JordanBuilder",
+    // 'age' => 25,
+    //])
+    //on pourra pas faire
+
+    // mais on pourra toujours faire l'assignation individuelle
+    //$post->name = "jordanBuilder"
+
+    protected $fillable = [
+        "invoice_number",
+        "total_vat",
+        "total_price_excluding_vat",
+        "total_price",
+    ];
+
+    protected $casts = [
+        //on caste en decimal avec 2 chiffres après la virgule
+        "total_vat" =>'decimal:2',
+        "total_price_excluding_vat" => 'decimal:2',
+        "total_price" => 'decimal:2',
+    ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+}
+
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        //times(5) pour créer 5 fake users pour les tests
+        User::factory()->times(5)->hasInvoice(10)->create();
+    }
+
+}
