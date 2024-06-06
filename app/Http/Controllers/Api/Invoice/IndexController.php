e29281206c5c1625e0401ae7591a55adb3bc2ade
jordan Tomegah <tomegahjordan81@gmail.com>
2024-06-06 18:58:34 +0000

    creation de la invoiceCollection,invoiceRessource et InvoiceCollectionResponse pour le formatage des reponses en JSON

--- app/Http/Controllers/Api/Invoice/IndexController.php
+++ app/Http/Controllers/Api/Invoice/IndexController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api\Invoice;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Invoice\InvoiceCollection;
+
 use App\Http\Responses\InvoiceCollectionResponse;
+use App\Http\Responses\InvoiceCollectionResponse as ResponsesInvoiceCollectionResponse;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 
@@ -16,15 +17,6 @@ class IndexController extends Controller
     public function __invoke(Request $request):  InvoiceCollectionResponse
     {
         //
-        return new InvoiceCollectionResponse(
-            collection: Invoice::query()
-            ->with([
-                'user'
-            ])
-            ->where('user_id', $request->user()->id)
-            ->paginate(25),
-            );
-
             return new InvoiceCollectionResponse(
                 collection: Invoice::query()
                 ->with([
---
+++ app/Http/Responses/InvoiceCollectionResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Responses;
+
+use App\Http\Resources\Invoice\InvoiceCollection;
+use App\Models\Invoice;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class InvoiceCollectionResponse implements Responsable
+{
+    protected $data;
+
+    public function __construct(
+        private readonly Collection|LengthAwarePaginator $collection,
+        private readonly int $status=200,
+    )
+    {
+
+    }
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        return response()->json(
+          data: new InvoiceCollection(
+            resource: $this->collection,
+          ),
+          status:$this->status,
+        );
+    }
+}
